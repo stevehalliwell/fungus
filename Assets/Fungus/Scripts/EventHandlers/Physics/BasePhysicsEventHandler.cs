8cd40f48b019db89f767d5f8030e95da207aa2cf
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-07-23 09:52:52 +1000

    Started to determine grouping of monobehaviour events Added Mouse EventHandler

---
+++ Assets/Fungus/Scripts/EventHandlers/Physics/BasePhysicsEventHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fungus
+{
+
+    /// <summary>
+    /// Base class for all of our physics event handlers
+    /// </summary>
+    [AddComponentMenu("")]
+    public abstract class BasePhysicsEventHandler : EventHandler
+    {
+        [System.Flags]
+        public enum PhysicsMessageType
+        {
+            Enter = 1 << 0,
+            Stay = 1 << 1,
+            Exit = 1 << 2,
+        }
+
+        [Tooltip("Only fire the event if one of the tags match. Empty means any will fire.")]
+        [SerializeField]
+        protected string[] tagFilter;
+
+        [Tooltip("Which of the 3d physics messages to we trigger on.")]
+        [SerializeField]
+        [EnumFlag]
+        protected PhysicsMessageType FireOn = PhysicsMessageType.Enter;
+
+        protected void ProcessCollider(PhysicsMessageType from, string tagOnOther)
+        {
+            if ((from & FireOn) != 0)
+            {
+
+                if (tagFilter.Length == 0)
+                {
+                    ExecuteBlock();
+                }
+                else
+                {
+                    if (System.Array.IndexOf(tagFilter, tagOnOther) != -1)
+                    {
+                        ExecuteBlock();
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Fungus/Scripts/EventHandlers/Physics/Trigger2D.cs
+++ Assets/Fungus/Scripts/EventHandlers/Physics/Trigger2D.cs
@@ -10,17 +10,9 @@ namespace Fungus
                       "Trigger",
                       "The block will execute when a 2d physics trigger matching some basic conditions is met.")]
     [AddComponentMenu("")]
-    public class Trigger2D : EventHandler
+    public class Trigger2D : BasePhysicsEventHandler
     {
-        [Tooltip("Only fire the event if one of the tags match. Empty means any will fire.")]
-        [SerializeField]
-        protected string[] tagFilter;
-
-        [Tooltip("Which of the 2d physics messages to we trigger on.")]
-        [SerializeField]
-        [EnumFlag]
-        protected PhysicsMessageType FireOn = PhysicsMessageType.Enter;
-
+      
         public override string GetSummary()
         {
             //TODO
@@ -29,41 +21,18 @@ namespace Fungus
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if ((FireOn & PhysicsMessageType.Enter) != 0)
-            {
-                ProcessCollider(col);
-            }
+            ProcessCollider(PhysicsMessageType.Enter, col.tag);
         }
         
         private void OnTriggerStay2D(Collider2D col)
         {
-            if ((FireOn & PhysicsMessageType.Stay) != 0)
-            {
-                ProcessCollider(col);
-            }
+            ProcessCollider(PhysicsMessageType.Stay, col.tag);
         }
 
         private void OnTriggerExit2D(Collider2D col)
         {
-            if ((FireOn & PhysicsMessageType.Exit) != 0)
-            {
-                ProcessCollider(col);
-            }
+            ProcessCollider(PhysicsMessageType.Exit, col.tag);
         }
 
-        private void ProcessCollider(Collider2D col)
-        {
-            if (tagFilter.Length == 0)
-            {
-                ExecuteBlock();
-            }
-            else
-            {
-                if (System.Array.IndexOf(tagFilter, col.tag) != -1)
-                {
-                    ExecuteBlock();
-                }
-            }
-        }
     }
 }
