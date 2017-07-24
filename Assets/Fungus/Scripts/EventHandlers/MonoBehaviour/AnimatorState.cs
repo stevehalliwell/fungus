845b652a76a25c5b9c5de40128d20d210e33ec63
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-07-24 20:17:20 +1000

    Refactor into tagfilteredeventhandler Added fungus event handlers for; transformChanged, Particle, CharacterControllerColliderHit, Animator

---
+++ Assets/Fungus/Scripts/EventHandlers/MonoBehaviour/AnimatorState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// The block will execute when the desired OnAnimator* message for the monobehaviour is received.
+    /// </summary>
+    [EventHandlerInfo("MonoBehaviour",
+                      "Animator",
+                      "The block will execute when the desired OnAnimator* message for the monobehaviour is received.")]
+    [AddComponentMenu("")]
+    public class AnimatorState : EventHandler
+    {
+
+        [System.Flags]
+        public enum AnimatorMessageFlags
+        {
+            OnAnimatorIK = 1 << 0,
+            OnAnimatorMove = 1 << 1,
+        }
+
+        [Tooltip("Which of the OnAnimator messages to trigger on.")]
+        [SerializeField]
+        [EnumFlag]
+        protected AnimatorMessageFlags FireOn = AnimatorMessageFlags.OnAnimatorMove;
+
+        [Tooltip("IK layer to trigger on")]
+        [SerializeField]
+        protected int IKLayer = 0;
+
+        public override string GetSummary()
+        {
+            //TODO
+            return "None";
+        }
+
+        private void OnAnimatorIK(int layer)
+        {
+            if ((FireOn & AnimatorMessageFlags.OnAnimatorIK) != 0 && IKLayer == layer)
+            {
+                ExecuteBlock();
+            }
+        }
+
+        private void OnAnimatorMove()
+        {
+            if ((FireOn & AnimatorMessageFlags.OnAnimatorMove) != 0)
+            {
+                ExecuteBlock();
+            }
+        }
+    }
+}
--- Assets/Fungus/Scripts/EventHandlers/MonoBehaviour/BasePhysicsEventHandler.cs
+++ Assets/Fungus/Scripts/EventHandlers/MonoBehaviour/BasePhysicsEventHandler.cs
@@ -8,7 +8,7 @@ namespace Fungus
     /// Base class for all of our physics event handlers
     /// </summary>
     [AddComponentMenu("")]
-    public abstract class BasePhysicsEventHandler : EventHandler
+    public abstract class BasePhysicsEventHandler : TagFilteredEventHandler
     {
         [System.Flags]
         public enum PhysicsMessageType
@@ -17,10 +17,6 @@ namespace Fungus
             Stay = 1 << 1,
             Exit = 1 << 2,
         }
-
-        [Tooltip("Only fire the event if one of the tags match. Empty means any will fire.")]
-        [SerializeField]
-        protected string[] tagFilter;
 
         [Tooltip("Which of the 3d physics messages to we trigger on.")]
         [SerializeField]
@@ -31,18 +27,7 @@ namespace Fungus
         {
             if ((from & FireOn) != 0)
             {
-
-                if (tagFilter.Length == 0)
-                {
-                    ExecuteBlock();
-                }
-                else
-                {
-                    if (System.Array.IndexOf(tagFilter, tagOnOther) != -1)
-                    {
-                        ExecuteBlock();
-                    }
-                }
+                ProcessTagFilter(tagOnOther);
             }
         }
     }
