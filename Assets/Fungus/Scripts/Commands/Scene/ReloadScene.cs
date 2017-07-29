08e771e65c9aadba46ad7a683193ae38d2f193db
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-07-29 13:36:46 +1000

    Added Reload Scene

---
+++ Assets/Fungus/Scripts/Commands/Scene/ReloadScene.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Reload the current scene
+    /// </summary>
+    [CommandInfo("Scene",
+                 "Clamp",
+                 "Reload the current scene")]
+    [AddComponentMenu("")]
+    public class ReloadScene : Command
+    {
+        [Tooltip("Image to display while loading the scene")]
+        [SerializeField]
+        protected Texture2D loadingImage;
+
+        public override void OnEnter()
+        {
+            SceneLoader.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, loadingImage);
+
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            return "Reload the current scene";
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+    }
+}
