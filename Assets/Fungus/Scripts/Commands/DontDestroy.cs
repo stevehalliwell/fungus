1d31f6fe4be81ef1abbad695472c3456bf15cd0b
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-08-02 21:25:24 +1000

    Added TakeScreenShot, DontDestroy

---
+++ Assets/Fungus/Scripts/Commands/DontDestroy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Mark a gameobject as dont destroy on load
+    /// </summary>
+    [CommandInfo("GameObject",
+                 "DontDestroyOnLoad",
+                 "Mark a gameobject as dont destroy on load.")]
+    [AddComponentMenu("")]
+    public class DontDestroy : Command
+    {
+        [Tooltip("GameObject to mark as Don'tDestroyOnLoad")]
+        [SerializeField]
+        protected GameObjectData gameObjectVar;
+
+        public override void OnEnter()
+        {
+            DontDestroyOnLoad(gameObjectVar.Value);
+
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            var g = gameObjectVar.Value; 
+
+            return "DontDestroy" + (g!=null ? g.name : "");
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+
+    }
+}
---
+++ Assets/Fungus/Scripts/Commands/TakeScreenshot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Save the current screen to a named png
+    /// </summary>
+    [CommandInfo("Camera",
+                 "Take Screenshot",
+                 "Save the current screen to a named png.")]
+    [AddComponentMenu("")]
+    public class TakeScreenshot : Command
+    {
+        [Tooltip("Filename to be saved under")]
+        [SerializeField]
+        protected StringData screenShotFileName = new StringData("screenshot");
+
+        [Tooltip("SuperSized screenshot")]
+        [SerializeField]
+        protected IntegerData superSized = new IntegerData(1);
+
+        public override void OnEnter()
+        {
+            Application.CaptureScreenshot(screenShotFileName.Value, superSized);
+
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            return "Screenshot";
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+
+    }
+}
