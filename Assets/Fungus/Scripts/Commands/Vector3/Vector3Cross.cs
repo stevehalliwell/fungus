8a72529d91c33c25fcfcce64463e446cab5bcbd0
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-09-10 21:21:31 +1000

    Added vec3dot and cross commands Removed old txt files

---
+++ Assets/Fungus/Scripts/Commands/Vector3/Vector3Cross.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Calculate the cross product of 2 vector3s.
+    /// </summary>
+    [CommandInfo("Vector3",
+                 "Cross",
+                 "Calculate the cross product of 2 vector3s.")]
+    [AddComponentMenu("")]
+    public class Vector3Cross : Command
+    {
+        [SerializeField]
+        protected Vector3Data lhs, rhs, output;
+
+        public override void OnEnter()
+        {
+            Vector3 tmp;
+
+            tmp = Vector3.Cross(lhs, rhs);
+
+            output.Value = tmp;
+            
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            if (output.vector3Ref == null)
+            {
+                return "Error: no output set";
+            }
+
+            return "Cross stored in " + output.vector3Ref.Key;
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+    }
+}
---
+++ Assets/Fungus/Scripts/Commands/Vector3/Vector3Dot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Calculate the dot product of 2 vector3s.
+    /// </summary>
+    [CommandInfo("Vector3",
+                 "Dot",
+                 "Calculate the cross product of 2 vector3s.")]
+    [AddComponentMenu("")]
+    public class Vector3Dot : Command
+    {
+        [SerializeField]
+        protected Vector3Data lhs, rhs;
+
+        [SerializeField]
+        protected FloatData output;
+        
+        [Tooltip("If true, lhs and rhs will be copied and normalised before dot product is calculated")]
+        [SerializeField]
+        protected bool normaliseInputs = false;
+
+        public override void OnEnter()
+        {
+            if(normaliseInputs)
+            {
+                output.Value = Vector3.Dot(lhs.Value.normalized, rhs.Value.normalized);
+            }
+            else
+            {
+                output.Value = Vector3.Dot(lhs, rhs);
+            }
+
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            if (output.floatRef == null)
+            {
+                return "Error: no output set";
+            }
+
+            return "Dot stored in " + output.floatRef.Key;
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+    }
+}
