506bcbe911208b8467f40dcd2060a9088a13ade1
MAESTY-MB\maesty <halliwell.steve@gmail.com>
2018-01-16 09:17:11 +1000

    Test custom var gen class

---
+++ Assets/FungusExamples/GeneratedVariables/ClassToGenerateFromTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassToGenerateFromTest : MonoBehaviour
+{
+    public string s;
+    public float f;
+    public int i;
+}
